fe5adb76f516e67c78882e626bc24f9f294bbaf0
WestleyArgentum <WestleyArgentum@gmail.com>
2015-03-26 23:08:45 -0400

    crude first step towards creating pass combinations

--- pass-ga.jl
+++ pass-ga.jl
@@ -3,12 +3,53 @@ module PassGA
 
 using GeneticAlgorithms
 
+passes = [
+    "createAddressSanitizerFunctionPass",
+    "createTypeBasedAliasAnalysisPass",
+    "createBasicAliasAnalysisPass",
+    "createCFGSimplificationPass",
+    "createPromoteMemoryToRegisterPass",
+    "createInstructionCombiningPass",
+    "createScalarReplAggregatesPass",
+    "createJumpThreadingPass",
+    "createReassociatePass",
+    "createEarlyCSEPass",
+    "createLoopIdiomPass",
+    "createLoopRotatePass",
+    "createLowerSimdLoopPass",
+    "createLICMPass",
+    "createLoopUnswitchPass",
+    "createIndVarSimplifyPass",
+    "createLoopDeletionPass",
+    "createLoopUnrollPass",
+    "createLoopVectorizePass",
+    "createGVNPass",
+    "createSCCPPass",
+    "createSinkingPass",
+    "createInstructionSimplifierPass",
+    "createJumpThreadingPass",
+    "createDeadStoreEliminationPass",
+    "createSLPVectorizerPass",
+    "createAggressiveDCEPass"
+]
+
 type PassMonster <: Entity
     passes::Array{String, 1}
     fitness
+
+    PassMonster() = new(Array(String, 1), nothing)
+    PassMonster(passes::Array{String, 1}) = new(passes, nothing)
 end
 
 function create_entity(num)
+    monster = PassMonster()
+
+    num_passes = rand(1:50)
+    for i in 1:num_passes
+        push!(monster.passes, passes[rand(1:length(passes))])
+    end
+
+    monster
 end
 
 function fitness(monster)
