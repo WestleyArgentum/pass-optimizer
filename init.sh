a2d93cffe739a6f41de0bc662e45b23779434789
WestleyArgentum <WestleyArgentum@gmail.com>
2015-04-12 16:48:39 -0400

    better init script

--- init.sh
+++ init.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-ln -sf ../../codegen/pass_setup.cpp ./julia/src/pass_setup.cpp
-ln -sf ../../codegen/codegen.cpp ./julia/src/codegen.cpp
+JULIA_DIR="./juliaa"
+
+if [ ! -d "$JULIA_DIR" ]; then
+    git clone https://github.com/JuliaLang/julia.git $JULIA_DIR
+fi
+
+ln -sf ../../codegen/pass_setup.cpp $JULIA_DIR/src/pass_setup.cpp
+ln -sf ../../codegen/codegen.cpp $JULIA_DIR/src/codegen.cpp
+
+cd $JULIA_DIR
+make
